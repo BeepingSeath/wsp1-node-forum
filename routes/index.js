09b307a21af062865509a1e10a104e7754f08f8e
BeepingSeath <Beeping.Seath.DKS@gmail.com>
2023-01-20 11:12:43 +0100

    o_o

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,31 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', async function (req, res) {
-    res.send('Hello you!')
+const mysql = require('mysql2');
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
 });
+const promisePool = pool.promise();
+router.get('/', async function (req, res, next) {
+    const [rows] = await promisePool.query("SELECT * FROM srb26forum");
+    res.json({ rows });
+});
+
+router.post('/new', async function (req, res, next) {
+    const { author, title, content } = req.body;
+    const [rows] = await promisePool.query("INSERT INTO srb26forum (author, title, content) VALUES (?, ?, ?)", [author, title, content]);
+    res.redirect('/');
+});
+router.get('/new', async function (req, res, next) {
+    const [users] = await promisePool.query("SELECT * FROM srb26users");
+    res.render('new.njk', {
+        title: 'Nytt inlägg',
+        users,
+    });
+});
+
 
 module.exports = router;
